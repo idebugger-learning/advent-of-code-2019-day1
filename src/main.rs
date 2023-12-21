51431ba8c963096463db5fc633c726927e83a141
iDeBugger Freeman <me@idebugger.io>
2023-12-21 22:21:41 +0100

    Part 2

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ fn main() {
         .collect();
 
     let fuel: usize = input.iter().map(fuel_for_mass).sum();
-    println!("[Part 1]");
+    println!("[Part 2]");
     println!("Fuel required: {}", fuel);
 }
 
 fn fuel_for_mass(mass: &usize) -> usize {
-    (mass / 3) - 2
+    let third = mass / 3;
+    if third >= 2 {
+        (third - 2) + fuel_for_mass(&(third - 2))
+    } else {
+        0
+    }
 }
